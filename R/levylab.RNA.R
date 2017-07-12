0e0a210bcf25f4bad625d7b500346d618915ef29
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-07-12 11:09:18 -0400

    Update authors & outline package main docs

---
+++ R/levylab.RNA.R
@@ -0,0 +1,12 @@
+#' levylab.RNA: A package for analysing risk & ambiguity task data
+#'
+#' @section levylab.RNA functions:
+#' \itemize{
+#' \item Import.
+#' \item Setup.
+#' \item Analysis.
+#' }
+#'
+#' @docType package
+#' @name levylab.RNA
+NULL
