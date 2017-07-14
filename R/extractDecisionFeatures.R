1fcaf1d04071b6633138bfe2a9124d6696983ed2
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-07-14 19:18:49 -0400

    Allow model-free computation even when there are NAs

--- R/extractDecisionFeatures.R
+++ R/extractDecisionFeatures.R
@@ -58,33 +58,42 @@ getModelBasedEstimates <- function(decision_data, initial_parameters = NULL) {
 #' Estimates risk and ambiguity aversion based on a model-free comparison-based approach
 #'
 #' @param decision_data A clean decision data frame
+#' @param na.rm Should the mean measures exclude NAs (and thus return non-NA results?)
 #' @return A data frame with risk_proportion, ambiguity_proportion and relative_ambiguity_proportion
 #'     parameters for each subject in decision_data.
 #' @import tidyverse
 #' @export
-getModelFreeEstimates <- function(decision_data) {
+getModelFreeEstimates <- function(decision_data, na.rm = TRUE) {
     # Proportion of lottery choices per subject + per probability/value combination
-    mf_risk_by.level.reward <- decision_data %>% filter(al == 0) %>% group_by(ID, p, val) %>% summarize(n = n(), proportion = mean(choice))
+    mf_risk_by.level.reward <- decision_data %>%
+      filter(al == 0) %>% group_by(ID, p, val) %>%
+      summarize(n = n(), proportion = mean(choice, na.rm = na.rm))
     head(mf_risk_by.level.reward)
 
     # Proportion of lottery choices per subject per probability level (without respect to value)
-    mf_risk_by.level <- mf_risk_by.level.reward %>% summarize(prop_p = mean(proportion))
+    mf_risk_by.level <- mf_risk_by.level.reward %>%
+      summarize(prop_p = mean(proportion, na.rm = na.rm))
     head(mf_risk_by.level)
 
     # Proportion of risky choices per subject (without respect to probability level)
-    mf_risk_all.levels <- mf_risk_by.level %>% summarize(risk_prop = mean(prop_p))
+    mf_risk_all.levels <- mf_risk_by.level %>%
+      summarize(risk_prop = mean(prop_p, na.rm = na.rm))
     mf_risk_all.levels
 
     # Proportion of lottery choices per subject + per ambiguity/value combination
-    mf_ambig_by.level.reward <- decision_data %>% filter(al > 0) %>% group_by(ID, al, val) %>% summarize(n = n(), proportion = mean(choice))
+    mf_ambig_by.level.reward <- decision_data %>% filter(al > 0) %>%
+      group_by(ID, al, val) %>%
+      summarize(n = n(), proportion = mean(choice, na.rm = na.rm))
     head(mf_ambig_by.level.reward)
 
     # Proportion of ambiguous choices per subject (without respect to value)
-    mf_ambig_by.level <- mf_ambig_by.level.reward %>% summarize(proportion = mean(proportion))
+    mf_ambig_by.level <- mf_ambig_by.level.reward %>%
+      summarize(proportion = mean(proportion, na.rm = na.rm))
     head(mf_ambig_by.level)
 
     # What proportion of all ambiguous choices did each subject select?
-    mf_ambig_all.levels <-  mf_ambig_by.level %>% summarize(ambig_prop = mean(proportion))
+    mf_ambig_all.levels <-  mf_ambig_by.level %>%
+      summarize(ambig_prop = mean(proportion, na.rm = na.rm))
     mf_ambig_all.levels
 
     # Now, looking at proportion of choices made at different ambiguity level *relative to* choices made at p = .5
@@ -97,7 +106,7 @@ getModelFreeEstimates <- function(decision_data) {
 
     # and without regard to ambiguity level
     mf_relative.ambig_all.levels <- group_by(mf_relative.ambig_by.level, ID) %>%
-        summarize(ambig_prop_relative = mean(diff))
+        summarize(ambig_prop_relative = mean(diff, na.rm = na.rm))
     mf_relative.ambig_all.levels
 
     # Pulling it together per subject
