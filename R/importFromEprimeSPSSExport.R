24bb6390b84fdbe7c126c10a75ec99871cf6a583
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-07-17 15:37:54 -0400

    Use quosures to implement the choiceColumnName argument

--- R/importFromEprimeSPSSExport.R
+++ R/importFromEprimeSPSSExport.R
@@ -8,17 +8,20 @@
 importFromEprimeSPSSExport <- function(filename, choiceColumnName = 'choice', discardOriginalColumns = FALSE) {
   # TODO: Ensure that it's not exported with Unicode? Use readr to detect it.
   # read.table('file.txt', header = TRUE, sep = '\t', as.is = TRUE, fileEncoding = 'UTF-16LE')
+  choice_name <- paste0(quo_name(choiceColumnName), '.RESP')
+
   x <- read.delim(filename, skip = 1)
   x <- mutate(x, payoff = ifelse(BlueValue == 0, RedValue, BlueValue),
               refSide = ifelse(LotterySide == 'Left', 2, 1)) %>%
-    rename(choice = choice.RESP) # FIXME: Should use rename_, NSE
+    rename(choice = !!choice_name) # FIXME: See rlang / "Programming with dplyr" vignette
   standard_cols <- getLevelsFromLotteryName(x$RiskAmbigLevel)
   ID <- as.numeric(str_match(filename, '\\d+')) # FIXME: Should attempt this, but fail gracefully
   standard_cols$ID <- ID
   x <- cbind(x, standard_cols)
 
   if (discardOriginalColumns) {
-    x <- select(x, -Block, -TrialNum, -BagNo, -BlueValue, -RedValue, -choice.RT, -LotterySide, -RiskAmbigLevel)
+    x <- select(x, -Block, -TrialNum, -BagNo, -BlueValue, -RedValue,
+                -ends_with('.RT'), -LotterySide, -RiskAmbigLevel)
     # FIXME: Should include rather than exclude
   }
   return(x)
@@ -27,11 +30,12 @@ importFromEprimeSPSSExport <- function(filename, choiceColumnName = 'choice', di
 #' From a filename of risk-and-ambiguity lottery display, extract kind and level of lottery
 #'
 #' @param name Filename in the format kind_winColor_numericalLevel (e.g. ambig_red_25.png)
+#' @import stringr
 #' @return A data frame with winProb, ambiguity, and winColor columns
 getLevelsFromLotteryName <- function(name) {
-  level <- as.numeric(str_match(name, '\\d+$'))
-  kind <- as.character(str_match(name, '^[a-z]+'))
-  winColor <- gsub('_', '', as.character(str_match(name, '_[a-z]+_')))
+  level <- as.numeric(stringr::str_match(name, '\\d+$'))
+  kind <- as.character(stringr::str_match(name, '^[a-z]+'))
+  winColor <- gsub('_', '', as.character(stringr::str_match(name, '_[a-z]+_')))
 
   ambig_trials <- kind == "ambig"
   p = level / 100
